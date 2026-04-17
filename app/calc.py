63319fd5f3cf52561fd77c5fa0e7c437ae182eb5
asmahmuallem-mmc <asmah.muallem@mmc.edu>
2026-04-16 19:22:00 -0500

    updated git ignore and added comments to calc.py

--- app/calc.py
+++ app/calc.py
@@ -1,6 +1,7 @@
+# Example: can raise TypeError (good for testing)
 def add(a: int, b: int) -> int:
     return a + b
-
+# Example: can raise OverflowError (good for testing)
 def div(a: float, b: float) -> float:
     # Example: can raise ZeroDivisionError (good for testing)
     return a / b
